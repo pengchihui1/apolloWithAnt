16504285f2b6c679e22dd4fe0a11a4c4737bce8c
XTC-02102162112\Administrator <pengchihui1@gmail.com>
2021-06-17 00:37:43 +0800

    '翻译'

--- modules/word/wordTranslationPage/index.js
+++ modules/word/wordTranslationPage/index.js
@@ -4,17 +4,31 @@ import {
   Textarea,
   Box
 } from '@chakra-ui/react'
-import Md5 from './md5.min'
 
 import { Container } from 'components/Container'
 import HeadTop from 'components/common/headTop'
 import ContainPage from 'components/containPage'
 import WordNav from '../wordNav'
 import http from './http'
-import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 const WordTranslationPage = () => {
-  // 免费api接口 https://www.jianshu.com/p/0182e59df879
+  const [data, setData] = useState('')
+  const [result, setResult] = useState([])
+
+  useEffect(() => {
+    http.get('/api/get_word', { params: { q: data } })
+      .then((res) => {
+        setResult(res.data.trans_result)
+        // console.log(res.data.trans_result)
+        // console.log(2222, result.length ? result[0].src : '')
+        // console.log(3333, result.length ? result[0].dst : '')
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [data])
+
   return (
     <Container height='100vh'>
       <ContainPage>
@@ -30,29 +44,15 @@ const WordTranslationPage = () => {
               size='sm'
               height='auto'
               onChange={(e) => {
-                const appid = '20210601000849775'
-                const key = 'uBb7ZcKZjtJ1aFQsWARw'
-                const from = 'auto'
-                const to = 'auto'
-                const query = 'word'// 取输入框的val
-                const q = encodeURIComponent(query)// 编码UTF-8
-                const salt = (new Date()).getTime()
-                const str1 = appid + query + salt + key// 秘钥
-                const sign = Md5(str1)// md5加密
-                axios.get('https://5b5e71c98e9f160014b88cc9.mockapi.io/api/v1/lists'
-                  //  { q, from, to, appid, salt, sign }
-                ).then(res => {
-                  console.log(res)
-                }).catch(function (error) {
-                  console.log(error)
-                })
+                setData(e.target.value)
               }}
             />
           </Box>
         </Flex>
         <Box>
-          <Text py={2}>内容：</Text>
-          <Text py={2}>读音：</Text>
+          <Text py={2}>单词：{result && result?.length ? result[0].src : ''}</Text>
+          <Text py={2}>翻译内容：{result && result?.length ? result[0].dst : ''}</Text>
+          {/* <Text py={2}>读音：</Text> */}
         </Box>
       </ContainPage>
     </Container>
